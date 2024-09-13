954b641c36f0853e7d0256cf352a5daf6310ded2
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-13 12:22:07 -0700

    Primitive vs Reference types, stack vs heap

--- 01-variables-data-types/04-data-types/myScript.js
+++ 01-variables-data-types/04-data-types/myScript.js
@@ -1,7 +1,6 @@
 // const firstName = 'Nick';
 // console.log(firstName, typeof firstName);
 
-
 // const obj = {
 //   name: 'Nick',
 //   occupation: {
@@ -20,5 +19,26 @@
 // obj1.name = 'Jonathan';
 // // Nested objects still reference original obj with spread operator
 // obj1.occupation.employment = 'Full-Time'
-
 // console.log(obj, obj1)
+
+
+/*  Primitive values are stored in the stack */
+
+// let oldName = 'Nick';
+// let newName = oldName;
+// newName = 'Jonathan';
+// console.log(oldName, newName)
+
+
+/*  Reference values are stored in the heap
+      Below, two variables point to the same object, in the heap
+      Variables are the address, object is the house, 2 variables with same address point to the same house
+*/
+
+// let oldPerson = {
+//   name: 'Nick'
+// }
+// let newPerson = oldPerson;
+// newPerson.name = 'Jonathan';
+// newPerson.age = 35;
+// console.log(oldPerson, newPerson);
