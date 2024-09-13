f85a506e42f5ecb913569cdb466683fc4474455a
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-13 11:41:10 -0700

    Data types practice

---
+++ 01-variables-data-types/04-data-types/myScript.js
@@ -0,0 +1,24 @@
+// const firstName = 'Nick';
+// console.log(firstName, typeof firstName);
+
+
+// const obj = {
+//   name: 'Nick',
+//   occupation: {
+//     employment: 'Unemployed',
+//     industry: 'Software',
+//   }
+// }
+// // console.log(obj);
+
+// // const obj1 = obj;
+// // obj1.age = 35;
+// // console.log(obj, obj1)
+
+// const obj1 = {...obj};
+// obj1.age = 35;
+// obj1.name = 'Jonathan';
+// // Nested objects still reference original obj with spread operator
+// obj1.occupation.employment = 'Full-Time'
+
+// console.log(obj, obj1)
