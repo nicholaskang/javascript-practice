29d37f0a05d0fe772497abc934f8d40659e261aa
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-13 13:13:29 -0700

    Operator practice

---
+++ 01-variables-data-types/07-operators/myScript.js
@@ -0,0 +1,30 @@
+// Concatenation
+// let greeting = 'Hello there';
+// let myName = 'Nick';
+// console.log(greeting + ', ' + myName);
+
+// Exponent
+// console.log(2 ** 3);
+
+// Assignment operator
+// x = 10;
+// x *= 5;
+// x /= 5;
+
+// Comparison operator
+// x = 2;
+// x = 2 == 2;
+// x = 2 == 4;
+// x = 2 === 2;
+// x = 2 == '2';
+// x = 2 === '2'; // checks type as well
+// x = 2 != 2;
+// x = 2 != 4;
+// x = 2 != '2';
+// x = 2 !== '2'; // checks type as well
+// x = 2 !== 2;
+
+// x = 10 > 5;
+// x = 5 >= 5;
+
+// console.log(x)
