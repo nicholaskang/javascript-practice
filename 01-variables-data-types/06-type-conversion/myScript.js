3cc9829bfc29eed0d9a74691a6a485b146527c83
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-13 12:45:25 -0700

    Type Conversion practice

---
+++ 01-variables-data-types/06-type-conversion/myScript.js
@@ -0,0 +1,41 @@
+/* 3 ways to convert STRING -> NUMBER
+    parseInt(), unary, Number()
+*/
+// let amount = '100';
+// amount = parseInt(amount);
+// amount = +amount;
+// amount = Number(amount);
+// console.log(amount, typeof amount);
+
+
+/* Ways to convert NUMBER --> STRING
+    .toString(), String()
+*/
+
+// let amount = 100;
+// amount = amount.toString();
+// amount = String(amount);
+// console.log(amount, typeof amount);
+
+/* Ways to convert STRING --> DECIMAL
+    parseFloat()
+*/
+
+// let amount = '99.5';
+// amount = parseNumber(amount); // returns rounded number
+// amount = parseFloat(amount); // returns 99.5 as number type
+// console.log(amount, typeof amount);
+
+/* Ways to convert STRING --> BOOLEAN
+    Boolean()
+*/
+
+// let amount = -1; // true
+// let amount = 0; // false
+// let amount = 1; // true
+// console.log(amount, typeof amount);
+
+// NaN
+// let amount = 'Hi';
+// amount = parseInt(amount);
+// console.log(amount
