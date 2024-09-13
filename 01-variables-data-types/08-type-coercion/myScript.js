0f47a35c072f784f0f6123870eb49ba1cb6b4801
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-13 13:20:58 -0700

    Type Coercion practice

---
+++ 01-variables-data-types/08-type-coercion/myScript.js
@@ -0,0 +1,12 @@
+// let x;
+// x = 5 + '5';         // returns 55 string
+// x = 5 + Number('5'); // returns 10 number
+// x = 5 * '5';         // returns 25 number
+// x = 5 + null;        // returns 5 number
+// x = Number(null);    // returns 0 number
+// x = Number(true);    // returns 1 number
+// x = Number(false);   // returns 0 number
+// x = 5 + true;        // returns 6 number
+// x = 5 + false;       // returns 5 number
+// x = 5 + undefined;   // returns NaN number
+// console.log(x, typeof x);
