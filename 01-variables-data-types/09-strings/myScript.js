611e403c873f168e0e487bfff3a1bb5b0e394c83
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-16 16:32:52 -0700

    String property/method practice

---
+++ 01-variables-data-types/09-strings/myScript.js
@@ -0,0 +1,42 @@
+let x;
+
+// const myName = 'Nick';
+// const myAge = 35;
+// // x = 'Hello my name is ' + myAge + ' and my age is ' + myAge;
+// x = `Hello my name is ${myAge} and my age is ${myAge}`;
+
+// String Properties and Methods
+
+const greeting = 'Hello World';
+// x = greeting.length; // length is a Property, doesn't need () suffix
+// x = greeting[0]; // Access value by key
+// x = greeting.__proto__ // Viewing prototype methods
+
+// x = greeting.toUpperCase();
+// x = greeting.toLowerCase();
+// x = greeting.match('l');
+// x = greeting.charAt(2);
+// x = greeting.indexOf('y');
+// x = greeting.substring(0, 4);
+// x = greeting.slice(0, 4);
+// x = greeting.slice(-11, -4);
+// const greeting = '        Hello World';
+// x = greeting.trim();
+// x = greeting.replace('World', 'John');
+// x = greeting.includes('World');
+// x = greeting.includes('llo');
+// x = greeting.valueOf();
+// x = greeting.split(' ');
+// x = greeting.split('');
+
+
+// // let newGreeting;
+// const removeSpace = (string) => {
+//   console.log(string.replace(' ', '')) 
+//   // newGreeting = string.replace(' ', '')
+// }
+// removeSpace(greeting);
+// // console.log(newGreeting)
+
+x = greeting;
+console.log(x)
