c966b77a530520721d40a86129fd4421d3e927c2
Nick Kang <nickkang257@gmail.com>
2024-09-16 17:54:26 -0700

    Number methods

---
+++ 01-variables-data-types/11-numbers/myScript.js
@@ -0,0 +1,22 @@
+// const newString = new String('Nick');
+// console.log(newString)
+
+let x;
+
+const num = new Number(5);
+
+// const num = 2;
+// const num = 20000000000;
+// const num = 5.67890;
+// const num = 15.67890;
+
+// x = num.toString();
+// x = x.length;
+
+// x = num.toFixed(3)
+// x = num.toPrecision(3)
+// x = num.toExponential(1);
+
+x = num;
+
+console.log(x)
