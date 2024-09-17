87b7df52f5a562a338281989e4ccae28182dc0ae
Nick Kang <nickkang257@gmail.com>
2024-09-16 18:04:26 -0700

    Math methods

---
+++ 01-variables-data-types/12-math-object/myScript.js
@@ -0,0 +1,19 @@
+let x;
+
+// x = Math.sqrt(25);
+// x = Math.abs(-25); // returns 25
+// x = Math.round(5.9);
+// x = Math.round(5.89); // returns 6
+// x = Math.ceil(4.67);
+// x = Math.ceil(4.1); // returns 5
+// x = Math.floor(4.9); // returns 4
+// x = Math.pow(2, 4); // does 2^4
+// x = Math.min(2, 4, 6);
+// x = Math.max(2, 4, 6);
+// x = Math.random();
+// x = Math.random() * 10;
+// x = Math.round(Math.random() * 10); // 0-9
+// x = Math.floor(Math.random() * 10 + 1); // 1-10 
+// x = Math.floor(Math.random() * 100 + 1); // 1-100
+
+console.log(x);
