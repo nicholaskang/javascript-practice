5fb4d6055dce0db79fffdc86e1685e6b8ddce04b
Nick Kang <nickkang257@gmail.com>
2024-09-16 17:19:44 -0700

    Capitalize first letter challenge

--- 01-variables-data-types/10-capitalize-challenge/myScript.js
+++ 01-variables-data-types/10-capitalize-challenge/myScript.js
@@ -1,10 +1,19 @@
-let myString = 'jonathan';
+const myString = 'jonathan';
+let myNewString;
 
 const capitalizeFirstLetter = (string) => {
-  let firstLetter = string[0].toUpperCase();
-  let remainingLetters = string.slice(1);
-  let newWord = firstLetter + remainingLetters;
-  console.log(newWord)
+  // Attempt 1
+  // let firstLetter = string[0].toUpperCase();
+  // let remainingLetters = string.slice(1);
+  // Attempt 2
+  // let newWord = firstLetter + remainingLetters;
+  // let newWord = string[0].toUpperCase() + string.slice(1);
+  // return newWord
+  // Attempt 3
+  // return string[0].toUpperCase() + string.slice(1);
+  // Attempt 4
+  return `${string[0].toUpperCase()}${string.slice(1)}`;
 }
 
-capitalizeFirstLetter(myString);
+myNewString = capitalizeFirstLetter(myString);
+console.log(myNewString)
