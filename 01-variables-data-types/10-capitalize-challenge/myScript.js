7d2db38737634ce4759a149db57319e3843ec595
Nick Kang <nickkang257@Nicks-MacBook-Pro.local>
2024-09-16 17:07:13 -0700

    Capitalize first letter challenge

---
+++ 01-variables-data-types/10-capitalize-challenge/myScript.js
@@ -0,0 +1,10 @@
+let myString = 'jonathan';
+
+const capitalizeFirstLetter = (string) => {
+  let firstLetter = string[0].toUpperCase();
+  let remainingLetters = string.slice(1);
+  let newWord = firstLetter + remainingLetters;
+  console.log(newWord)
+}
+
+capitalizeFirstLetter(myString);
