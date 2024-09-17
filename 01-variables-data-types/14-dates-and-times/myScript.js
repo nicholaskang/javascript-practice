2f9d54d66685db5978c96ff03ae5d26ae9f68af7
Nick Kang <nickkang257@gmail.com>
2024-09-16 18:38:14 -0700

    Date Time Intro/Practice

---
+++ 01-variables-data-types/14-dates-and-times/myScript.js
@@ -0,0 +1,12 @@
+let d;
+
+d = new Date();
+// d = new Date(2024, 5, 23, 6,23);
+d = new Date('2024-6-23');
+d = new Date('06/23/1989');
+d = Date.now();
+// d = d.getTime();
+// d = d.valueOf();
+d = new Date(1726537027130);
+
+console.log(d)
