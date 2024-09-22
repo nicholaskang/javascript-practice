abf80135eb6560d6b0d9b0f72bd5107d264420d1
Nick Kang <nickkang257@gmail.com>
2024-09-21 18:44:39 -0700

    Array basics

---
+++ 02-arrays-and-objects/01-array-basics/myScript.js
@@ -0,0 +1,22 @@
+let x;
+
+// Array Literal
+const numbers = [1,2,3,4,5,6];
+// console.log(numbers[0]);
+
+// Array Constructor
+const fruits = new Array('apple', 'grape', 'orange');
+// console.log(fruits)
+
+// x = numbers[0];
+// x = numbers[0] + numbers[3];
+// x = numbers.length;
+
+// fruits[1] = 'pear';
+// console.log(fruits);
+
+// fruits[fruits.length] = 'strawberry';
+// fruits.push('strawberry');
+console.log(fruits)
+
+// console.log(x);
