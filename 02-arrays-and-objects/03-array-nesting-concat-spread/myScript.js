b4a96347217cba4001476593659ce26d4924a624
Nick Kang <nickkang257@gmail.com>
2024-09-21 19:20:19 -0700

    Array advanced methods

---
+++ 02-arrays-and-objects/03-array-nesting-concat-spread/myScript.js
@@ -0,0 +1,23 @@
+let x;
+const fruits = ['apple', 'pear', 'orange'];
+const berries = ['strawberry', 'blueberry', 'rasberry'];
+
+// fruits.push(berries);
+// x = fruits[3];
+// x = fruits[3][1];
+
+// x = fruits.concat(berries)
+// x = fruits.concat('strawberry')
+
+// x = [...fruits, ...berries]
+
+// const arr = [1,2,3,[4,5,6]];
+// x = arr.flat()
+
+// x = Array.isArray(fruits)
+// x = Array.from('12345');
+
+// const a = 1, b = 2, c = 3;
+// x = Array.of(a, b, c)
+
+console.log(x);
